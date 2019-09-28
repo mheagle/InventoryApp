8fb7e92038a338c3e064b9d92563c6fcd3b442b4
mheagle <55907008+mheagle@users.noreply.github.com>
2019-09-27 22:26:39 -0700

    Initial commit

---
+++ InventoryApp/InventoryApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InventoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var element = new Element
+            {
+                Material = "Material",
+                Weight = 123,
+                WSPrice = 123.45M,
+                Location = "location",
+            };
+            element.WSWeightValue();
+
+            Console.WriteLine($"InventoryNo:  {element.InventoryNumber}," 
+                $"Material: { element.Material}," +
+                $"  Grams: { element.Weight}" );
+        }
+}
